a9a9e4fb8a6c82c2ba273d93470c3bda8cd8c1e6
santhoshm <santomaharaj@gmail.com>
2020-12-03 00:37:25 +1100

    New Test file added

---
+++ src/test/java/com/javainuse/SpringBootHelloWorldApplicationTests.java
@@ -0,0 +1,14 @@
+package com.javainuse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootHelloWorldApplicationTests {
+    @Test
+    public void contextLoads() {
+
+    }
+}
